cb10d4ce9c97f380a634e5419c93d21ce9c3d2eb
USA_UK VS <L@lcrypto.com>
2022-11-07 15:03:33 +0300

    Add files via upload

--- sparse2alist.m
+++ sparse2alist.m
@@ -1,65 +1,50 @@
-function sparse2alist(Q, H, file_name)
-    [M, N] = size(H);
-    file = fopen(file_name, 'w');
-    
-    if Q==2
-        fprintf(file, '%d %d\n', N, M);
-    else
-        fprintf(file, '%d %d %d\n', N, M, Q);
-    end
-    
-    column_weights = sum(full((H>0)));
-    row_weights = sum(full((H>0)'));
-    c_max = max(column_weights);
-    r_max = max(row_weights); 
-    
-    fprintf(file, '%d %d\n', c_max, r_max);
-    
-    fprintf(file, '%d ', column_weights(1:end-1));
-    fprintf(file, '%d\n', column_weights(end));
-    
-    fprintf(file, '%d ', row_weights(1:end-1));
-    fprintf(file, '%d\n', row_weights(end));
-    
-    for i = 1:N
-        indexes = (find(H(:,i)))';
-        values = (full(H(indexes, i)))';
-        if Q == 2
-            temp = indexes;
-        else
-            temp = [indexes; values];
-        end
-        fprintf(file, '%d ', temp);
-        if length(indexes) < c_max
-            if Q == 2
-                fillers = zeros(1, c_max - length(indexes));
-            else
-                fillers = zeros(1, 2*(c_max - length(indexes)));
-            end
-            fprintf(file, '%d ', fillers);
-        end
-        fprintf(file, '\n');
-    end
-    
-    for i = 1:M
-        indexes = find(H(i,:));
-        values = full(H(i, indexes));
-        if Q == 2
-            temp = indexes;
-        else
-            temp = [indexes; values];
-        end
-        fprintf(file, '%d ', temp);
-        if length(indexes) < r_max
-            if Q == 2
-                fillers = zeros(1, r_max - length(indexes));
-            else
-                fillers = zeros(1, 2*(r_max - length(indexes)));
-            end
-            fprintf(file, '%d ', fillers);
-        end
-        fprintf(file, '\n');
-    end
-    
-    fclose(file);
+function sparse2alist(H,file)
+
+A = full(H);
+
+M = size(A,1);
+N = size(A,2);
+
+ratebyrows = zeros(M,1);
+ratebycols = zeros(N,1);
+
+for i = 1:M
+    rateinrow = find(A(i,:));
+    ratebyrows(i) = size(rateinrow,2);
 end
+
+
+for i = 1:N
+    rateincol = find(A(:,i));
+    ratebycols(i) = size(rateincol,1);
+end
+
+maxinrows = max(ratebyrows);
+maxincols = max(ratebycols);
+
+%fID = fopen("alist.txt", 'w');
+fID = fopen(file, 'w');
+fprintf(fID,'%d\t', M, N);
+fprintf(fID,'\n');
+fprintf(fID,'%d\t', maxinrows, maxincols);
+fprintf(fID,'\n');
+fprintf(fID,'%d\t', ratebyrows);
+fprintf(fID,'\n');
+fprintf(fID,'%d\t', ratebycols);
+fprintf(fID,'\n');
+
+for i = 1:M
+    rateinrow = find(A(i,:));
+    fprintf(fID,'%d\t', rateinrow);
+    fprintf(fID,'\n');
+end
+
+for i = 1:N
+    rateincol = find(A(:,i));
+    fprintf(fID,'%d\t', rateincol);
+    fprintf(fID,'\n');
+end
+
+fclose(fID);
+
+
